70a503bdaa2c00612d5dfb3adb7f855b683800cf
belalbou <belalbou48@gmail.com>
2024-10-05 11:37:07 +0200

    Structure MVC inital

--- app/views/templates/partials/_main.php
+++
@@ -1,1 +0,0 @@
-<?php include "../app/views/templates/partials/_aside.php"; ?>
--- core/init.php
+++ core/init.php
@@ -2,4 +2,3 @@
 
 require_once "../app/config/params.php";
 require_once "../core/connexion.php";
-// require_once "../core/helpers";
